915b8a3a332613aeba60ecb82ea941344ffe24f3
Michael Tolan <mtolan@gmail.com>
2015-04-20 23:08:09 -0400

    Removed commented code & unused variable

--- test/cookbooks/awsmetadata/recipes/default.rb
+++ test/cookbooks/awsmetadata/recipes/default.rb
@@ -2,11 +2,6 @@ package 'python-virtualenv'
 package 'python-pip'
 package 'supervisor'
 
-# apt-get update
-# execute "loopback1" do
-# 	command "ifconfig lo:1 169.254.169.254 netmask 255.255.255.0 up"
-# end
-
 execute "create virtualenv" do
 	command "virtualenv /usr/local/mockaws"
 	not_if { File.exists?('/usr/local/mockaws') }
@@ -25,7 +20,6 @@ template "/usr/local/mockaws/mockawsm.py" do
   source "mockawsm.py.erb"
   action :create_if_missing
   variables({
-    :device_list => node['awsmetadata']['device_list'],
     :block_device_json => node['awsmetadata']['block_device_json']
   })
 end
@@ -39,15 +33,3 @@ cookbook_file "mockaws.conf" do
   action :create_if_missing
   notifies :restart, "service[supervisor]"
 end
-
-
-
-# apt-get install -y python-virtualenv python-pip supervisor
-
-# virtualenv /usr/local/mockaws
-# mv /tmp/mockaws/* /usr/local/mockaws
-
-# /usr/local/mockaws/bin/pip install -r /usr/local/mockaws/requirements.txt
-
-# echo -e "[program:mockawsm]\ncommand=/usr/local/mockaws/bin/python /usr/local/mockaws/mockawsm.py" > /etc/supervisor/conf.d/mockaws.conf
-# supervisorctl reload
